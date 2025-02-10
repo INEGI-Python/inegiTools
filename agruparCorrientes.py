d2e163946bd93b52c2c45dd1075191f0b017efb4
emmanuel <emmanuel.rodriguez@inegi.org.mx>
2025-02-10 16:28:13 -0600

    cuasi

--- agruparCorrientes.py
+++ agruparCorrientes.py
@@ -2,76 +2,80 @@ import geopandas as geo
 from shapely.geometry import Point,LineString
 import rtree
 
-class puntoInicial:
-	def __init__(self,i,p):
-		self.id_punto=i
-		self.punto=p
-		self.ramaIni=None
-		self.buscaRamas()
-
-	def buscaRamas(self):
-		tmp = list(rtreeIdx.intersection(self.punto.bounds))
-		lin = lineas.iloc[tmp]
-		for g  in lin.geometry:
-			ini,fin=Point(*g.coords[0]),Point(*g.coords[-1])
-			self.ramaIni =  rama(tmp[0],ini if fin == self.punto else fin)
-
-class rama:
-	def __init__(self,id,pts):
-		self.id = id
-		self.punto = pts
-		self.subramas = self.buscaSubramas()
-	def buscaSubramas(self):
-		setUsados(self.id)
-		tmp = [idx for idx in  list(rtreeIdx.intersection(self.punto.bounds))  if idx not in usadas]
-		print(tmp)
-		ramTmp=[]
-		if len(tmp)>0:
-			lin = lineas.iloc[tmp]
-			for  ir,l in  lin.iterrows():
-				print(l)
-				ini,fin = Point(*l.geometry.coords[0]),Point(*l.geometry.coords[-1])
-				new_punto = ini if fin == self.punto else fin
-				ramTmp.append(rama(ir,new_punto))
-			return ramTmp
-		return None
 
 
+class corrientes(object):
+	def __init__(self,**a):
+		self.lineas = []
+		self.CRS=None
+		self.tipo=a["tipo"]
+		self.rtreeIdx = None
+		self.importar(a["ruta"])
 
-def build_arboles(puntos):
-    arboles = []
-    usadas = set()
-    stack = []
-    for i, row in puntos.iterrows():
-        stack.append(Rama(i, row.geometry, usadas))
-    while stack:
-        rama_node = stack.pop()
-        arboles.append(rama_node)
-        if rama_node.subramas:
-            stack.extend(rama_node.subramas)
-    return arboles
+	def importar(self,ruta):
+		tmp = geo.read_file(ruta,columns=["geometry"])
+		self.CRS = tmp.crs.to_string()
+		for i,l in tmp.iterrows():
+			lin=dict(id=i+1,geometry=l.geometry,start=Point(*l.geometry.coords[0]),end=Point(*l.geometry.coords[-1]),grupo=-1,orden=-1,flip=-1)
+			self.lineas.append(lin)
+		self.rtreeIdx = self.indiceLineas(tmp)
+		
+	def indiceLineas(self,gdf):
+		idx =  rtree.index.Index()
+		reng = [l.bounds for l in gdf.geometry]
+		for  i , bbox in  enumerate(reng):
+			idx.insert(i,bbox)
+		return  idx
+
+	def aguasArriba(self,id,gpo):
+		sub = list(self.rtreeIdx.intersection(self.lineas[id]["start"].bounds))
+		ramas,res = [self.lineas[i]["id"] for i in sub if self.lineas[i]["id"] != id],[]
+		if len(ramas)==0:
+			return self.lineas[id]["id"]
+		for s in sub:
+			self.lineas[s]["grupo"] = gpo
+			self.lineas[s]["orden"] = self.lineas[id]["orden"]+1
+			if self.lineas[id]["start"].distance(self.lineas[s]["end"]) > 0:
+				self.lineas[s]["geometry"] = LineString(self.lineas[s]["geometry"].coords[::-1])
+				self.lineas[s]["flip"] = 1
+			else:
+				self.lineas[s]["flip"] = 0
+			res.append(self.aguasArriba(s,gpo))
+		return res
+class puntoInicial(object):
+	def __init__(self,**a):
+		self.puntos=[]
+		self.CRS=None
+		self.tipo=a["tipo"]
+		self.importar(a["ruta"],a["hidro"])
+
+	def importar(self,ruta,h):
+		tmp = geo.read_file(ruta,columns=["geometry"])
+		self.CRS = tmp.crs.to_string()
+		for i,p in tmp.iterrows():
+			pun=dict(id=i+1,geometry=p.geometry,linea=list(h.rtreeIdx.intersection(p.geometry.bounds))[0])
+			if pun["geometry"].distance(h.lineas[pun["linea"]]["end"]) > 0:
+				h.lineas[pun["linea"]]["gemeotry"]=LineString(h.lineas[pun["linea"]]["geometry"].coords[::-1])
+				h.lineas[pun["linea"]]["flip"]=1
+			else:
+				h.lineas[pun["linea"]]["flip"]=3
+			h.lineas[pun["linea"]]["grupo"]=pun["id"]
+			h.lineas[pun["linea"]]["orden"]=1
+			self.puntos.append(pun)
 
 
+hidro = corrientes(tipo="LINESTRING",ruta="datos/corrientesAgua.shp")
+puntos = puntoInicial(tipo="POINT",ruta="datos/puntos_dren.shp",hidro=hidro)
+arboles=[h["id"]  for h in hidro.lineas]
+arch = open("datos/arboles.txt","w")	
+arch.write(str(arboles))
+arch.close()
 
-def setUsados(id):
-	usadas.append(id)
-	print(usadas)
+for p in puntos.puntos:
+	arboles.extend(hidro.aguasArriba(p["linea"],p["id"]))
 
-def indiceLineas(ruta,cant=None,campos=None):
-	gdf = geo.read_file(ruta,rows=cant,columns=campos)
-	idx =  rtree.index.Index()
-	reng = [l.bounds for l in gdf.geometry]
-	for  i , bbox in  enumerate(reng):
-		idx.insert(i,bbox)
-	return  idx, gdf
+print(len(arboles))
 
-usadas=[]
-rtreeIdx, lineas = indiceLineas("datos/corrientesAgua.shp",None,["geometry"])
-puntos = geo.read_file("datos/puntos_dren.shp",columns=["geometry"])
-arboles = []
-for i,p in puntos.iterrows():
-	arboles.append(puntoInicial(i,p.geometry))
-
-print(arboles)
-#a1 = arbol(1,[1,1])
+misLineas = hidro.lineas[:150]
+misPuntos = puntos.puntos[:150]
 
